1417c35605760359349b52593dfc9195c1c2ba73
Gian Lorenzo Meocci <glmeocci@gmail.com>
2012-01-22 17:13:10 +0100

    prima dello splash

---
+++ assets/www/multilingua.js
@@ -0,0 +1,13 @@
+var Language = {
+  'it' : {
+    datetime: "Data e Ora:",
+    sunrise: "Alba:",
+    sunset: "Tramonto:",
+    solar_noon : "Culminazione:",
+    select_date : "Seleziona data",
+    tomorrow_sunrise: "Alba di domani:",
+    tomorrow_sunset: "Tramonto di domani:",
+    longitude: "Longitudine:",
+    latitude: "Latitudine:"
+  }
+};
